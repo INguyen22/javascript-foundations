29e6e3692c73b80b8e2a0886a7972d4ad2ad6534
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-18 15:01:15 -0600

    work on dragon test

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,18 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name
+    this.rider = rider
+    this.hungry = true
+    this.
+    }
 
+  greet() {
+    return `Hi, ${this.rider}!`
+  }
+
+  eat(nom) {
+    }
+
+  }
+
+module.exports = Dragon;
