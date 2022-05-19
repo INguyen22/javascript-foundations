dc930f736e8a652ef8e0db74620e131f7a1f77fe
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-19 13:13:09 -0600

    do vampire test

--- mythical-creatures/exercises/vampire.js
+++ mythical-creatures/exercises/vampire.js
@@ -2,7 +2,24 @@ class Vampire {
   constructor(name, otherPet) {
     this.name = name
     this.pet = otherPet || 'bat'
+    this.thirsty = true
+    this.drank = true
+    this.ouncesDrank = 0
+  }
+  drink() {
+    this.ouncesDrank = this.ouncesDrank + 10
+    for (var i)
+    if ((this.drank === true) && (this.ouncesDrank === 50)) {
+      this.thirsty = false
+      return 'I\'m too full to drink anymore'
+    }
   }
 }
+
+/*this.ouncesDrank = this.ouncesDrank + 10
+console.log(this.ouncesDrank)
+if (this.ouncesDrank === 50) {
+this.thristy = false
+return 'I\'m too full to drink anymore'*/
 
 module.exports = Vampire;
