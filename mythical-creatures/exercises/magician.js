bcf59e5bda459498e6802a22c8b5634ca2ac5276
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-20 15:09:12 -0600

    finish magician

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -5,16 +5,27 @@ class Magician {
     this.name = 'The Great ' + mage.name
     this.assistant = mage.assistant
     this.favoriteAccessory = mage.clothing || 'top hat'
+    this.confidencePercentage = 10
   }
   performIncantation(spell) {
     return spell.toUpperCase() + '!'
   }
   performTrick() {
-    console.log('trick', performTrick)
-    return 'meh'
+    this.confidencePercentage += 10
+    if (this.favoriteAccessory === 'top hat') {
+    return 'PULL RABBIT FROM TOP HAT'
+    }
+    return 'PULL DOVE FROM SLEEVE'
+  }
+  performShowStopper() {
+    if (this.confidencePercentage >= 100 && this.assistant === true) {
+      console.log(this.confidencePercentage)
+      return 'WOW! The magician totally just sawed that person in half!'
+      }
+      return 'Oh no, this trick is not ready!'
   }
 }
 
-var mage = new Magician({})
+//var mage = new Magician({})
 
 module.exports = Magician;
