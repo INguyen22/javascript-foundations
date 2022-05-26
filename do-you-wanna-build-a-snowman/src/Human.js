e92b84df738a3d6083bee9df864a7008dc7adca8
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-26 17:52:15 -0600

    add snowman to placeMagicHat functio in test. finsih snow man and ski lift

--- do-you-wanna-build-a-snowman/src/Human.js
+++ do-you-wanna-build-a-snowman/src/Human.js
@@ -1,7 +1,32 @@
 var Snowman = require('./Snowman');
 
 class Human {
-
+  constructor(name) {
+    this.name = name
+    this.wantsToBuildASnowman = true
+    this.materials = {
+      snowballs: 0,
+      coal: 0,
+      buttons: 0,
+      carrots: 0
+    }
+  }
+  gatherMaterials(materials, num) {
+    this.materials[materials] = this.materials[materials] + num
+  }
+  buildASnowman() {
+    console.log('materials', this.materials)
+    var snowman = new Snowman(this.materials)
+    return snowman
+  }
+  placeMagicHat(snowman) {
+    if (snowman.magicHat) {
+    //if (this.buildASnowman(this.materials).magicHat) {
+    //if (this.materials.coal >= 2 && this.materials.buttons >= 5 && this.materials.carrots >= 1 && this.materials.snowballs >= 2) {
+    return 'Woah, this snowman is coming to life!'
+  }
+  return 'I guess I didn\'t build it correctly.'
+  }
 }
 
 module.exports = Human;
--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,10 +1,19 @@
 class Snowman {
   constructor(snowmanDetails) {
-    console.log('snowman', snowmanDetails)
+    //console.log('snowman', snowmanDetails)
     this.carrots = snowmanDetails.carrots;
     this.coal = snowmanDetails.coal;
     this.buttons = snowmanDetails.buttons;
     this.snowballs = snowmanDetails.snowballs
+    this.magicHat = false
+    }
+    canWearMagicHat() {
+      console.log('this', this)
+      //console.log('buttons', this.buttons)
+      if (this.coal >= 2 && this.buttons >= 5 && this.carrots >= 1 && this.snowballs >= 2) {
+      this.magicHat = true
+      }
+      console.log('magichat', this.magicHat)
     }
   }
 
