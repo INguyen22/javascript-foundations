e67681f02754cd6577c0a70f176b266ff4acab50
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-25 21:16:22 -0600

    Finsih centaur test

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -4,12 +4,43 @@ class Centaur {
     this.breed = centaurDetails.type
     this.cranky = false
     this.standing = true
+    this.layingDown = false
+    this.activity = 0
   }
   shootBow() {
-    return this.cranky = true, "Twang!!!"
+    if (this.activity < 2) {
+    this.activity += 1
+    return "Twang!!!"
+  }
+    return this.cranky = true, "NO!"
   }
   run(){
-    return this.cranky = true, "Clop clop clop clop!!!"
+    if (this.activity < 2) {
+    this.activity += 1
+    return "Clop clop clop clop!!!"
+  }
+    return this.cranky = true, "NO!"
+  }
+  sleep() {
+    if (this.standing === true) {
+      return 'NO!'
+    }
+    return this.cranky = false, this.activity = 0, 'ZZZZ'
+  }
+  layDown() {
+    this.standing = false
+    this.layingDown = true
+    this.activity = 2
+  }
+  standUp() {
+    this.standing = true
+    this.layingDown = false
+  }
+  drinkPotion() {
+    if (this.standing === true) {
+    this.cranky = false
+    }
+    return 'Not while I\'m laying down!'
   }
 }
 
