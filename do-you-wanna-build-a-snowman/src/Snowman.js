4a307d2fc4af6b22c9891ff1be09c4b3bc861785
Ivy Nguyen <animeweirdo24@gmail.com>
2022-05-26 09:24:59 -0600

    start snowman test

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,12 @@
 class Snowman {
+  constructor(snowmanDetails) {
+    console.log('snowman', snowmanDetails)
+    this.carrots = snowmanDetails.carrots;
+    this.coal = snowmanDetails.coal;
+    this.buttons = snowmanDetails.buttons;
+    this.snowballs = snowmanDetails.snowballs
+    }
+  }
 
-}
 
 module.exports = Snowman;
