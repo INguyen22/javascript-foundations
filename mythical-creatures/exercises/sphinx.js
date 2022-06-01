c18e4fe5ccc8939756ce3240f803e6498be2ec06
Ivy Nguyen <animeweirdo24@gmail.com>
2022-06-01 17:44:35 -0600

    Do sphinx

--- mythical-creatures/exercises/sphinx.js
+++ mythical-creatures/exercises/sphinx.js
@@ -8,17 +8,17 @@ class Sphinx {
     this.riddles.push(riddle)
     if (this.riddles.length === 4) {
       this.riddles.shift()
-      return this.riddles
     }
   }
   attemptAnswer(answer) {
     for (var i = 0; i < this.riddles.length; i++) {
     if (this.riddles[i].answer.includes(answer)) {
       this.riddles.splice(i, 1)
+      //console.log('riddles', this.riddles)
       return 'That wasn\'t that hard, I bet you don\'t get the next one'
       }
-      this.heroesEaten += 1
     }
+    this.heroesEaten ++
   }
 }
 
