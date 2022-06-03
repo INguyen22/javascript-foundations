07525d3748744f7540baaa1490726026bb8395ad
Ivy Nguyen <animeweirdo24@gmail.com>
2022-06-03 13:50:50 -0600

    Do material test

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,21 @@
 class Material {
-
+ constructor(name, price, amount, units) {
+   this.name = name
+   this.price = price
+   this.amount = amount
+   this.units = units
+ }
+ useMaterial(amountUsed) {
+   if (amountUsed > this.amount) {
+     return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+   }
+   this.amount = this.amount - amountUsed
+     return `You now have ${this.amount} ${this.units} of ${this.name} left.`
+ }
+ calculateMaterialCost() {
+   var newPrice = this.price * this.amount
+   return newPrice
+ }
 }
 
 module.exports = Material;
