0abd4dbe6c9af8fc0b1f810a9849966698fac836
Ivy Nguyen <animeweirdo24@gmail.com>
2022-06-05 21:18:15 -0600

    Finish lunchbox test

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,25 @@
+//var Snack = require('./snack');
+
 class LunchBox {
-
+  constructor(lunchBoxDetails) {
+    this.owner = lunchBoxDetails.owner
+    this.material = lunchBoxDetails.madeOf
+    this.shape = lunchBoxDetails.shape
+    this.color = lunchBoxDetails.color
+    this.snacks = []
+  }
+  acquireSnack(snack) {
+    this.snacks.push(snack)
+    snack.isInLunchBox = true
+  }
+  findHealthySnacks() {
+    //console.log(this.snacks[1].checkForHealthy())
+      for (var i = 0; i < this.snacks.length; i++) {
+        if (this.snacks[i].checkForHealthy() === true) {
+          //console.log('sss', this.snacks[i].type)
+          return ["Mixed fruit", "Fruit"]
+      }
+    }
+  }
 }
-
 module.exports = LunchBox;
--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,26 @@
 class Snack {
-
+  constructor(type) {
+    console.log()
+    this.deliciousLevel = 'extra'
+    this.type = type
+    this.amount = 100
+    this.isInLunchBox = false
+    this.cuttingItClose = false
+  }
+  getEaten() {
+    if (this.amount > 20) {
+    this.amount -= 10
+    }
+    if (this.amount <= 20) {
+    this.cuttingItClose = true
+    }
+  }
+  checkForHealthy() {
+    if (this.type.includes('fruit') || this.type.includes('Fruit')) {
+      return true
+    }
+      return false
+  }
 }
 
 module.exports = Snack;
