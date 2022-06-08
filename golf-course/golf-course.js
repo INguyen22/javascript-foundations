3ed7492227bb729005c3c0dc46d6791700a893cc
Ivy Nguyen <animeweirdo24@gmail.com>
2022-06-08 15:49:30 -0600

    Finish golf course

--- golf-course/golf-course.js
+++ golf-course/golf-course.js
@@ -1,6 +1,50 @@
+var Golfer = require('./golfer.js')
+
 class GolfCourse {
-
-
+  constructor(name, difficulty, openings, features) {
+    this.name = name
+    this.difficulty = difficulty
+    this.openings = openings
+    this.features = features
+    this.currentlyPlaying = []
+  }
+  checkInGroup(golfer) {
+    for (var i = 0; i < golfer.length; i++) {
+      if (this.openings < golfer.length) {
+        return 'Sorry, we are currently booked! Please come back later.'
+      }
+      if (this.openings > golfer.length) {
+        this.currentlyPlaying.unshift(golfer[i].name)
+      }
+  }
+  this.openings -= golfer.length
+  return 'You\'re checked in. Have fun!'
 }
+}
+  // checkInGroup(golfer) {
+  //   for (var i = 0; i < golfer.length; i++) {
+  //     if (golfer.length === 3) {
+  //   this.currentlyPlaying.unshift(golfer[i].name)
+  //   console.log('golfer', this.currentlyPlaying)
+  //     }
+  //     if (golfer.length === 2) {
+  //       return 'Sorry, we are currently booked! Please come back later.'
+  //     }
+  //   }
+  //   this.openings -= golfer.length
+  //   return 'You\'re checked in. Have fun!'
+  // }
 
+// checkInGroup() {
+//   for (var i = 0; i < golfer.length; i++) {
+//     if (this.openings < golfer.length) {
+//       return 'Sorry, we are currently booked! Please come back later.'
+//     }
+//     if (this.openings > golfer.length) {
+//       this.currentlyPlaying.unshift(golfer[i].name)
+//     }
+// }
+// this.openings -= golfer.length
+// return 'You\'re checked in. Have fun!'
+// }
 module.exports = GolfCourse;
--- golf-course/golfer.js
+++ golf-course/golfer.js
@@ -1,6 +1,45 @@
 class Golfer {
-
-
+  constructor(golferDetails) {
+    this.name = golferDetails.name
+    this.handicap = golferDetails.handicap
+    this.frustration = 0
+    this.confidence = 0
+  }
+  calculateAvg(par) {
+    //console.log('par', par)
+    this.handicap += par
+    return `I usually shoot a ${this.handicap} on average.`
+  }
+  playRound(course) {
+    //console.log(course)
+    if (course.difficulty === 'hard') {
+      this.frustration += 500
+    }
+    if (course.difficulty === 'moderate') {
+    this.frustration += 100
+    }
+  }
+  hitTheRange() {
+    this.confidence += 10
+  }
+  marvel(course) {
+    //console.log('course', course)
+    return `I love the ${course.features[0]} and ${course.features[1]} on this course!`
+  }
+  whatYaShoot(score) {
+    if (score === 0) {
+      this.frustration -= 10
+      return 'Booyah!'
+    }
+    else if (score < 0) {
+      this.frustration = 0
+      return 'I AM THE GREATEST GOLFER ALIVE!'
+    }
+    else {
+      this.frustration += 20
+      return 'Doh!'
+    }
+  }
 }
 
 module.exports = Golfer;
