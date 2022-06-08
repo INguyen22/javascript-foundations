d2f8077adf81857e6c54da350dfc0a08afa9d9f1
Ivy Nguyen <animeweirdo24@gmail.com>
2022-06-07 18:55:48 -0600

    Finish skatepark

--- skate-park/skatepark.js
+++ skate-park/skatepark.js
@@ -8,6 +8,23 @@ class SkatePark{
     this.cost = skateparkDetails.price || 0
     this.occupants = []
   }
+  admit(skater) {
+    console.log('skater', skater)
+    if (!this.isPrivate) {
+      this.occupants.push(skater)
+      return 'Welcome to the free Major Taylor Skatepark!'
+    }
+    if (this.isPrivate && skater.money >= 12 && this.occupants.length < 3) {
+        this.occupants.push(skater)
+        skater.money = skater.money - 12
+        return 'Welcome to Curbside, the cost will be $12.00.'
+    }
+    else if (this.isPrivate && skater.money < 12) {
+      return `Sorry, you don't have enough money.`
+    }
+    else if (this.isPrivate && this.occupants.length === 3)
+    return 'Sorry, we are at max capacity. Thank you for understanding.'
+}
 }
 
 module.exports = SkatePark;
--- skate-park/skater.js
+++ skate-park/skater.js
@@ -1,6 +1,22 @@
 class Skater {
   constructor(skaterDetails) {
-    this.name = 
+    this.name = skaterDetails.name
+    this.skill = skaterDetails.skill
+    this.tricks = skaterDetails.tricks
+    this.money = skaterDetails.cash
+    this.frustration = 0
+  }
+  practice(trick) {
+    //console.log('tricks', trick)
+    //console.log('tricks', this.tricks[trick])
+    if (!this.tricks[trick] && this.frustration < 2) {
+    this.frustration += 1
+    }
+    else {
+      this.frustration = 0
+      this.tricks[trick] = true
+      return `I just learned to ${trick}!!!`
+    }
   }
 }
 
